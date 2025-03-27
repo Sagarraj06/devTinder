89f4ec20c24c183a7517c2588d03c1d11fda2f3f
Sagarraj06 <sr8936050368@gmail.com>
2025-03-27 18:29:05 +0530

    second

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
+app.get("/user", (req,res)=>{
+    res.send({ firstname:"Sagar",lastname:"Raj"});
+})
+app.post("/user",(req,res)=>{
+res.send("Data successfully saved to the database!");
 });
 
-app.use("/",(req,res)=>{
-    res.send("Welcome to home page");
-});
+app.delete("/user",(req,res)=>{
+    res.send(" Delete successfully ")}
+);
 
+
+
+// this will match all the http method API call to /test
+app.use("/Test",(req,res)=>{
+    res.send("Welcome to our server");
+});
 
 
 app.listen(7777,()=>{
