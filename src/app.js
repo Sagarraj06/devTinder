ad5dff0e4c750209e1822254c8ebf8597af53180
Sagarraj06 <sr8936050368@gmail.com>
2025-04-03 06:39:22 +0530

    sixth

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const connectDB = require ("./config/database");
 const User = require("./models/user");
+const { validateSignUp } = require("./utils/validation");
 const app=express();
+const bcrypt = require("bcrypt"); 
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 // const {adminAuth, userAuth} = require("./middlewares/auth");
 
@@ -17,12 +21,26 @@ const app=express();
 // });
 app.use(express.json());
 
+app.use(cookieParser());
+
 
 // post route to add user
 app.post("/signup", async (req, res) => {
+
+    try{
+        // Validate the request body
+        validateSignUp(req);
+
+
+        const {password,firstName,lastName,emailId} = req.body;
+        // Encription of password
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash); // Log the hashed password
         
-        const user = await User.create(req.body);
-try{
+        const user = await User.create({
+            firstName, lastName,emailId,password:passwordHash,
+        });
+
         //console.log(user); // Log the created user
         res.send("User Added Successfully");
     } catch (error) {
@@ -77,6 +95,7 @@ app.patch("/update/:userId", async (req,res)=>{
 
     
     try{
+         
         const ALLOWED_UPDATE=[
             "photoUrl", "about" , "gender" , "age", "skills" , "firstName", "lastName", "password"
         ];
@@ -100,7 +119,69 @@ app.patch("/update/:userId", async (req,res)=>{
         console.error("Error updating user:", err);
         res.status(500).send("An error occurred while updating the user.");
     }
-})
+});
+
+app.post("/login", async (req,res)=>{
+    try{
+        const { emailId , password}= req.body;
+        // if(!validator.isEmail(emailId)){
+        //     throw new Error("Email is not valid");
+        // }
+        const user = await User.findOne({emailId});
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+
+        if(isPasswordValid){
+
+            const token = await jwt.sign({_id:user._id}, "SagarDevTinder");
+            console.log(token); // Log the generated token
+            res.cookie("token", token );
+            res.send("Login successful");
+        }
+        else{
+            res.send("Invalid Credentials");
+        }
+}
+catch(err){
+    console.error("Error logging in:", err);
+    res.status(400).send("Error : " + err.message);
+}
+});
+   
+
+app.get("/profile", async(req,res)=>{
+    try {
+        // Read cookies from the request
+        const cookies = req.cookies;
+        console.log("Cookies:", cookies); 
+
+        const { token } = cookies;
+        if(!token){
+            throw new Error("Token not found in cookies");
+        }
+        console.log("Token:", token); 
+        // Verify the token
+        const decodedmsg = await jwt.verify(token, "SagarDevTinder");
+        const _id = decodedmsg._id;
+        console.log("Decoded ID:", _id); // Log the decoded ID
+        console.log("Decoded JWT:", decodedmsg); // Log the decoded token
+        const user = await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        res.send("User:"+ user); // Log the user object
+
+        res.send("Profile accessed successfully");
+    }
+ 
+catch(err){
+    console.error("Error fetching profile:", err);
+    res.status(500).send("An error occurred while fetching the profile.");
+}
+});
 
 
 
@@ -114,4 +195,4 @@ connectDB()
 })
 .catch((err)=>{
 console.log("Error connecting to database", err);
-});  
+});   
---
+++ src/utils/validation.js
@@ -0,0 +1,22 @@
+const validator = require("validator");
+
+const validateSignUp= (req)=>{
+    const {firstName, lastName, emailId, password}= req.body;
+    if(!firstName){
+        throw new Error("First name is required");
+    }
+    else if(!lastName){
+        throw new Error("Last name is requires");
+    }
+    else if(firstName.length<4 || firstName.length<4 || lastName.length>50 || firstName.length>50){
+        throw new Error("First name and last name should be between 4 to 50 characters");
+    }
+    else if(!validator.isEmail(emailId)){
+        throw new Error("Email is not valid");
+    }
+    else if(!validator.isStrongPassword(password)){
+        throw new Error("Password is not strong enough");
+    }
+};
+
+module.exports = {validateSignUp};
